d94df1f2816e3150412548e34f1019004edc3fac
Qqkyu <kulmisiu007@gmail.com>
2021-12-28 13:47:39 +0100

    Expense addition e2e tests

---
+++ cypress/integration/transactions/expense_addition_spec.js
@@ -0,0 +1,95 @@
+import { deleteAllBills } from '../../utils/bills-utils';
+
+describe('expense addition', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('can create new expense with provided amount and default date', () => {
+    cy.get('.account-widget').should('not.exist');
+    const newAccName = 'expense test account';
+    cy.get('.account-form input[placeholder="Account name"]').type(newAccName);
+
+    cy.get('.account-form div:nth-child(2) div:nth-child(2) input').type('100');
+
+    cy.get('button')
+      .contains('Save Account')
+      .click();
+    cy.get('.account-widget').should('exist');
+    cy.get('.account-widget-account__name a').should(
+      'contain.text',
+      newAccName
+    );
+    cy.get('button')
+      .contains('Finish')
+      .click();
+
+    cy.get('div.account-widget-account__name').should('exist');
+    cy.get('div.account-widget-account__name a').contains(newAccName);
+
+    cy.get('div.account-widget-account__balance').should('exist');
+    cy.get('div.account-widget-account__balance span').contains('100,00');
+    cy.get('div.account-widget-account__balance span').contains('USD');
+
+    cy.visit('http://localhost:3000/transactions');
+    cy.wait(1000);
+
+    cy.get('button')
+      .contains('New')
+      .click();
+
+    cy.get('a')
+      .contains('Expense')
+      .click();
+
+    cy.get('form.transaction-form div:nth-child(1) input')
+      .first()
+      .type('50');
+
+    cy.get('button')
+      .contains('Add Expense')
+      .click();
+
+    cy.get('td')
+      .contains('Total expense')
+      .parent()
+      .children('td:nth-child(2)')
+      .children('span')
+      .contains('50,00')
+      .should('exist');
+  });
+
+  it('can create new expense with provided amount and custom date', () => {
+    cy.visit('http://localhost:3000/transactions');
+    cy.wait(1000);
+
+    cy.get('button')
+      .contains('New')
+      .click();
+
+    cy.get('a')
+      .contains('Expense')
+      .click();
+
+    cy.get('form.transaction-form div:nth-child(1) input')
+      .first()
+      .type('50');
+
+    cy.get('form.transaction-form input[type="date"]').type('1999-12-31');
+
+    cy.get('button')
+      .contains('Add Expense')
+      .click();
+
+    cy.get('td')
+      .contains('Total expense')
+      .parent()
+      .children('td:nth-child(2)')
+      .children('span')
+      .contains('50,00')
+      .should('exist');
+
+    cy.wait(500);
+    deleteAllBills();
+  });
+});
