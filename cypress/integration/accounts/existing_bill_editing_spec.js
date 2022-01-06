1c434a947d388b96d7982e05925f8ac1a9c90057
Qqkyu <kulmisiu007@gmail.com>
2022-01-06 16:05:54 +0100

    Existing bill editing e2e tests

---
+++ cypress/integration/accounts/existing_bill_editing_spec.js
@@ -0,0 +1,87 @@
+import { deleteAllBills } from '../../utils/bills-utils';
+
+describe('existing bill editing', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('displays existing bill edition fields', () => {
+    cy.get('.account-widget').should('not.exist');
+    const newAccName = 'acc';
+    cy.get('.account-form input[placeholder="Account name"]').type(newAccName);
+    cy.get('button')
+      .contains('Save Account')
+      .click();
+    cy.get('button')
+      .contains('Finish')
+      .click();
+
+    cy.get('.account-widget').should('exist');
+    cy.get('.account-widget-account__name a').should(
+      'contain.text',
+      newAccName
+    );
+
+    cy.visit('http://localhost:3000/accounts');
+    cy.wait(1000);
+
+    cy.get('.account-widget-account__edit button').click();
+
+    cy.get('.account-form').should('exist');
+    cy.get('.account-form .fields').should('exist');
+
+    cy.get('.account-form div:nth-child(1) div:nth-child(1) label').should(
+      'contain.text',
+      'Name'
+    );
+    cy.get('.account-form div:nth-child(1) div:nth-child(2) label').should(
+      'contain.text',
+      'Group'
+    );
+
+    cy.get('.account-form div:nth-child(3) button').should(
+      'contain.text',
+      'Save Account'
+    );
+
+    cy.wait(1000);
+    deleteAllBills();
+  });
+
+  it('displays existing bill edition fields', () => {
+    cy.get('.account-widget').should('not.exist');
+    const newAccName = 'acc';
+    cy.get('.account-form input[placeholder="Account name"]').type(newAccName);
+    cy.get('button')
+      .contains('Save Account')
+      .click();
+    cy.get('button')
+      .contains('Finish')
+      .click();
+
+    cy.get('.account-widget').should('exist');
+    cy.get('.account-widget-account__name a').should(
+      'contain.text',
+      newAccName
+    );
+
+    cy.visit('http://localhost:3000/accounts');
+    cy.wait(1000);
+
+    cy.get('.account-widget-account__edit button').click();
+
+    cy.get('.account-form input[placeholder="Account name"]').type(' edited');
+
+    cy.get('button')
+      .contains('Save Account')
+      .click();
+
+    cy.get('.account-widget').should('exist');
+    cy.get('.account-widget-account__name a')
+      .contains('acc edited')
+      .should('exist');
+
+    cy.wait(1000);
+    deleteAllBills();
+  });
+});
